b9f3572bb94ff9f5a191c42ef271dbf1b5cb06fa
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-10-02 23:01:39 +0530

    Project Begin with basic server setup and testing

---
+++ track_my_cash/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import  Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    </>
+  );
+}
+
+export default App;
