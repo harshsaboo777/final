11aa0894cd518c1aaad71bfa0e3c5ef35fe6bbfe
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-10-03 16:31:05 +0530

    Added basic and group components

--- track_my_cash/src/App.js
+++ track_my_cash/src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import  Navbar from './components/Navbar';
+import SignUp from './components/SignUp';
+
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
     <Navbar />
+    {/* <SignUp /> */}
+    <Routes>
+      <Route exact path="/SignUp" element={<SignUp />}/>
+
+      
+    </Routes>
     </>
   );
 }
---
+++ track_my_cash/src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const SignUp = () => {
+    return (
+        <React.Fragment>
+         <div className='container'> 
+            
+         </div>
+        </React.Fragment>)
+}
+
+export default SignUp
