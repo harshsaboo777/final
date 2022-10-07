aa25cefeb868a452c9dae6bb655dc894c9904d62
hussaindiwan52 <hussaindiwan52@gmail.com>
2022-10-08 00:26:35 +0530

    Added Floating Sidebar

--- track_my_cash/client/src/components/Group_Dashboard.js
+++ track_my_cash/client/src/components/Group_Dashboard.js
@@ -1,67 +1,71 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import "../componentsStyles/group_dashboard.css"
+import Floating_Sidebar from "./Floating_Sidebar";
+import "../componentsStyles/group_dashboard.css";
 const GroupDashBoard = () => {
-    return (
-        <React.Fragment>
-            <div>
-                <Sidebar />
-                <div className="container">
-                    <div className="card">
-                        <div className="card-header">
-                            Featured
-                        </div>
-                        <div className="card-body">
-                        <div className="row">
+	return (
+		<React.Fragment>
+			<div>
+				<Sidebar />
+				<Floating_Sidebar />
 
+				<div className="container ">
+					<div className="card">
+						<div className="card-header">Featured</div>
+						<div className="card-body">
+							<div className="row">
+								<div className="col-md-12">
+									<div className="card detail-card">
+										<div className="card-body">
+											This is some text within a card
+											body.
+										</div>
+									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											This is some text within a card
+											body.
+										</div>
+									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											This is some text within a card
+											body.
+										</div>
+									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											This is some text within a card
+											body.
+										</div>
+									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											This is some text within a card
+											body.
+										</div>
+									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											This is some text within a card
+											body.
+										</div>
+									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											This is some text within a card
+											body.
+										</div>
+									</div>
+								</div>
+								{/* <div className="col-md-1 border"></div> */}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
 
-<div className="col-md-11">
-    <div className="card detail-card">
-        <div className="card-body">
-            This is some text within a card body.
-        </div>
-    </div>
-    <div className="card detail-card">
-        <div className="card-body">
-            This is some text within a card body.
-        </div>
-    </div>
-    <div className="card detail-card">
-        <div className="card-body">
-            This is some text within a card body.
-        </div>
-    </div>
-    <div className="card detail-card">
-        <div className="card-body">
-            This is some text within a card body.
-        </div>
-    </div>
-    <div className="card detail-card">
-        <div className="card-body">
-            This is some text within a card body.
-        </div>
-    </div>
-
-
-</div>
-<div className="col-md-1 border">
-
-</div>
-
-
-</div>
-                        </div>
-                    
-
-
-                    
-                    </div>
-                </div>
-
-            </div>
-        </React.Fragment>
-
-    )
-}
-
-export default GroupDashBoard
+export default GroupDashBoard;
