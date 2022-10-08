d790debc29f2640b1eab3b1aed75f40e4d399743
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-10-08 22:47:26 +0530

    Group dashboard completed

--- track_my_cash/client/src/components/Floating_Sidebar.js
+++ track_my_cash/client/src/components/Floating_Sidebar.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "../componentsStyles/floating_sidebar.css";
-const Floating_Sidebar = () => {
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';import BalanceIcon from '@mui/icons-material/Balance';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';const Floating_Sidebar = () => {
 	return (
 		<>
 			<div id="floating-panel2">
-				<h1>P1</h1>
-				<h1>P2</h1>
-				<h1>P3</h1>
-				<h1>P4</h1>
+				<div className="floating-icon">
+				<InfoOutlinedIcon fontSize="large" sx={{color:'#ffff'}}/>
+
+				</div>
+				<div className="floating-icon">
+				<AddCircleOutlineOutlinedIcon fontSize="large" sx={{color:'#ffff'}}/>
+
+				</div>
+				<div className="floating-icon">
+				<BalanceIcon fontSize="large" sx={{color:'#ffff'}}/>
+
+				</div>
+				
+				<SettingsIcon fontSize="large" sx={{color:'#ffff'}}/>
+
+				
+				{/* <AddCircleIcon fontSize="large"/> */}
+				
+				{/* <h1>P4</h1> */}
 			</div>
 		</>
 	);
--- track_my_cash/client/src/components/Group_Dashboard.js
+++ track_my_cash/client/src/components/Group_Dashboard.js
@@ -9,7 +9,7 @@ const GroupDashBoard = () => {
 				<Sidebar />
 				<Floating_Sidebar />
 
-				<div className="container ">
+				<div className="container mt-4">
 					<div className="card">
 						<div className="card-header">Featured</div>
 						<div className="card-body">
@@ -17,46 +17,131 @@ const GroupDashBoard = () => {
 								<div className="col-md-12">
 									<div className="card detail-card">
 										<div className="card-body">
-											This is some text within a card
-											body.
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
 										</div>
 									</div>
 									<div className="card detail-card">
 										<div className="card-body">
-											This is some text within a card
-											body.
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
 										</div>
 									</div>
 									<div className="card detail-card">
 										<div className="card-body">
-											This is some text within a card
-											body.
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
 										</div>
 									</div>
 									<div className="card detail-card">
 										<div className="card-body">
-											This is some text within a card
-											body.
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
 										</div>
 									</div>
 									<div className="card detail-card">
 										<div className="card-body">
-											This is some text within a card
-											body.
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
 										</div>
 									</div>
 									<div className="card detail-card">
 										<div className="card-body">
-											This is some text within a card
-											body.
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
 										</div>
 									</div>
 									<div className="card detail-card">
 										<div className="card-body">
-											This is some text within a card
-											body.
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
 										</div>
 									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
+										</div>
+									</div>
+									<div className="card detail-card">
+										<div className="card-body">
+											<div className="row detail-head">
+												<div className="col-md-10"> Memeber 1</div>
+												<div className="col-md-2 d-flex justify-content-end">225.00 INR</div>
+
+											</div>
+											<div className="row mt-2 detail-foot ">
+												<div className="col-md-10"> Party</div>
+												<div className="col-md-2 d-flex justify-content-end">31 Jan,2021</div>
+
+											</div>
+										</div>
+									</div>
+									
 								</div>
 								{/* <div className="col-md-1 border"></div> */}
 							</div>
