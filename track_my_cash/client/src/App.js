a9fe1aa34e6d57e64af1f508c85ae539e87d7e24
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-10-10 18:46:25 +0530

    Evaluation page added

--- track_my_cash/client/src/App.js
+++ track_my_cash/client/src/App.js
@@ -4,9 +4,11 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Navbar from "./components/Navbar";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
+import Select_Path from "./components/Select_Path";
 import GroupDashBoard from "./components/Group_Dashboard";
 
 import { Routes, Route } from "react-router-dom";
+import Evaluation from "./components/Evaluation";
 
 function App() {
 	return (
@@ -17,6 +19,8 @@ function App() {
 				<Route exact path="/SignUp" element={<SignUp />} />
 				<Route exact path="/Login" element={<Login />} />
 				<Route exact path="/Group" element={<GroupDashBoard />} />
+				<Route exact path="/Select" element={<Select_Path />} />
+				<Route exact path="/Evaluate" element={<Evaluation />} />
 			</Routes>
 		</>
 	);
---
+++ track_my_cash/client/src/components/Select_Path.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+function Select_Path() {
+  return (
+    <div>
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Select_Path
