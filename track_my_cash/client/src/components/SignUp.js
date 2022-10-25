eb8039056b02bb6196ebb0d9bb588d8ed8c701d6
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-10-25 16:12:44 +0530

    SignUp Page created and Linked

--- track_my_cash/client/src/components/SignUp.js
+++ track_my_cash/client/src/components/SignUp.js
@@ -5,35 +5,94 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import GoogleIcon from "@mui/icons-material/Google";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const SignUp = () => {
+	const navigate = useNavigate();
+	const [user, setuser] = useState({
+		Fname: "",
+		Lname: "",
+		DOB: "",
+		Phone_Num: "",
+		DOB: "",
+		Salary: "",
+		email: "",
+		password: "",
+		Cpassword: "",
+	});
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setuser({
+			...user,
+			[name]: value,
+		});
+	};
 	return (
 		<React.Fragment>
 			<div className={styles.container}>
 				<div className={styles.loginbox}>
-					<p className={styles.logintext}>Login with social media</p>
-
-					<div className={styles.loginsocial}>
-						<a href="#" className={styles.facebook}>
-							<FacebookIcon /> Facebook{" "}
-						</a>
-						<a href="#" className={styles.twitter}>
-							<TwitterIcon /> Twitter{" "}
-						</a>
-						<a href="#" className={styles.google}>
-							<GoogleIcon /> Google{" "}
-						</a>
-						<a href="#" className={styles.linkedin}>
-							<LinkedInIcon /> Linkedin{" "}
-						</a>
-					</div>
 
 					<form method="POST" className={styles.loginemail}>
-						<p className={styles.logintext}>Login with email</p>
+						<p className={styles.logintext}>SignUp with email</p>
 						<div className={styles.inputgroup}>
 							<input
 								type="text"
 								placeholder="First Name"
 								name="Fname"
+								value={user.Fname}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						<div className={styles.inputgroup}>
+							<input
+								type="text"
+								placeholder="Last Name"
+								name="Lname"
+								value={user.Lname}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						<div className={styles.inputgroup}>
+							
+							<input
+								type="date"
+								placeholder="Date of Birth"
+								name="DOB"
+								value={user.DOB}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						<div className={styles.inputgroup}>
+							<input
+								type="tel"
+								placeholder="Phone Number"
+								name="Phone_Num"
+								value={user.Phone_Num}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						<div className={styles.inputgroup}>
+							<input
+								type="number"
+								placeholder="Salary"
+								name="Salary"
+								value={user.Salary}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						
+						<div className={styles.inputgroup}>
+							<input
+								type="password"
+								placeholder="Password"
+								name="Password"
+								value={user.password}
+								onChange={handleChange}
 								required
 							/>
 						</div>
@@ -41,8 +100,10 @@ const SignUp = () => {
 						<div className={styles.inputgroup}>
 							<input
 								type="password"
-								placeholder="Password"
-								name="Password"
+								placeholder="Confirm Password"
+								name="Cpassword"
+								value={user.Cpassword}
+								onChange={handleChange}
 								required
 							/>
 						</div>
@@ -52,8 +113,10 @@ const SignUp = () => {
 							</button>
 						</div>
 						<p className={styles.loginregistertext}>
-							Don't have an account?{" "}
-							<a href="register.php">Register Here</a>
+							Already have an account?{" "}
+							<button onClick={() => navigate("/Login")}>
+									Sign In
+								</button>
 						</p>
 					</form>
 				</div>
