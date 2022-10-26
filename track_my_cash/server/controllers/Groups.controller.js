ec8485b3d5dbf7116ea17eddaeeeb89141b3acb1
hussaindiwan52 <hussaindiwan52@gmail.com>
2022-10-26 22:57:06 +0530

    add group backend added

--- track_my_cash/server/controllers/Groups.controller.js
+++ track_my_cash/server/controllers/Groups.controller.js
@@ -27,3 +27,24 @@ export const showExpenses = async (req, res) => {
 	}
 	res.status(200).send(expenses);
 };
+
+export const addGroup = async (req, res) => {
+	let mem_id = req.body.mem_id;
+	let group_id;
+	try {
+		await client.query(
+			"INSERT INTO Groups(Owner_id,Name,Created_on) VALUES ($1,$2,NOW())",
+			[parseInt(mem_id), group_name]
+		);
+		group_id = await client.query(
+			"Select group_id from groups where owner_id=$1 and name=$2",
+			[parseInt(mem_id), group_name]
+		);
+		await client.query(
+			"INSERT INTO belongs_to(mem_id,group_id,amount_due) VALUES ($1,$2,0)",
+			[parseInt(mem_id), parseInt(group_id)]
+		);
+	} catch (err) {
+		console.log(err);
+	}
+};
