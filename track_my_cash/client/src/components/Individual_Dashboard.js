9c0c61ff55697977c663ea3e543a70b891ad19f2
harshsaboo777 <gs2010073@sgsitsindore.in>
2022-11-03 14:39:39 +0530

    Show individual expense

--- track_my_cash/client/src/components/Individual_Dashboard.js
+++ track_my_cash/client/src/components/Individual_Dashboard.js
@@ -7,38 +7,64 @@ import members from "./tempMembers";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Cookies from "universal-cookie";
-
-// import members from "./Add_Group_expense";
+import Update_salary from "./update_salary"
+import Add_Individual_expense from "./Add_Individual_expense";
+import Individual_card from "./Individual_card";
 
 const cookies = new Cookies();
 const Expenses = [];
 const IndividualDashBoard = () => {
 	const Mem_Id = cookies.get("Member").mem_id;
-	const [member, setMember] = useState({});
-	const [membersExpenses, setmembersExpenses] = useState(Expenses);
+	const [member, setMember] = useState({});//fname lname salary
+	const [membersExpenses, setmembersExpenses] = useState([]);
+	const [overall_expense,setoverall_expense] = useState(0);
+	const [salary, setsalary] = useState(member.salary);
 	const onChangeState = (newState) => {
 		setmembersExpenses(newState);
 		// console.log(membersArr);
 	};
+
 	const fetchMember = async (e) => {
 		await axios
 			.get("http://localhost:5000/member/" + Mem_Id)
 			.then((res) => {
 				setMember(res.data);
 				console.log(res.data);
+				setsalary(res.data.salary);
 			});
 	};
+
+	const fetchexpenses = async () =>{
+		await axios
+			.get("http://localhost:5000/member/expenses/" + Mem_Id)
+			.then((res) => {
+				setmembersExpenses(res.data);
+			});
+	}
+
+	const settotexpense = ()=>{
+		    let temp=0;
+			for(let i=0;i<membersExpenses.length;i++)
+			{
+				temp=temp+parseInt(membersExpenses[i].amount);
+			}
+			console.log(temp);
+			setoverall_expense(temp);
+	}
 	useEffect(() => {
 		fetchMember();
-	}, []);
+		fetchexpenses();
+		settotexpense();
+	},);
 
+	console.log(membersExpenses)
 	return (
-		<React.Fragment>
+		<React.Fragment >
 			<div>
 				<Sidebar />
 
 				<div className="container mt-4">
-					<div className="card">
+					<div className="card ">
 						{/* <div className="card-header">Featured</div> */}
 						<div className="card-body">
 							<div className="row">
@@ -46,21 +72,25 @@ const IndividualDashBoard = () => {
 									<div className="card detail-card">
 										<div className="card-body">
 											<div className="row detail-head">
-												<div className="col-md-10">
+												<h3 className="col-md-10">
 													{" "}
 													{member.fname +
 														" " +
 														member.lname}
-												</div>
+												</h3>
 											</div>
 											<div className="row detail-head">
 												<div className="col-md-8">
 													{" "}
-													Salary: {member.salary}
+													<h5 >Salary : {salary}</h5> 
+													<div><Update_salary Mem_Id={Mem_Id} setsalary={setsalary}/></div>
+													<h6 className="mt-5">Savings: {salary-overall_expense}.00</h6>
 												</div>
+												
 												<div className="col-md-4 d-flex justify-content-end">
-													Monthly Limit: 500000
+													<h5>Total Expense: {overall_expense}.00</h5>
 												</div>
+												
 											</div>
 										</div>
 									</div>
@@ -68,156 +98,15 @@ const IndividualDashBoard = () => {
 								{/* <div className="col-md-1 border"></div> */}
 							</div>
 						</div>
+						<Add_Individual_expense/>
 					</div>
 					<div className="card mt-4">
 						<div className="card-header">Expenses</div>
-						<div className="card-body">
-							<div className="card detail-card">
-								<div className="card-body">
-									<div className="row detail-head">
-										<div className="col-md-10">
-											{" "}
-											Grocery
-										</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											500 INR
-										</div>
-									</div>
-									<div className="row mt-2 detail-foot ">
-										<div className="col-md-10"> Food</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											10/12/22
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<div className="card detail-card">
-								<div className="card-body">
-									<div className="row detail-head">
-										<div className="col-md-10">
-											{" "}
-											Grocery
-										</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											500 INR
-										</div>
-									</div>
-									<div className="row mt-2 detail-foot ">
-										<div className="col-md-10"> Food</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											10/12/22
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<div className="card detail-card">
-								<div className="card-body">
-									<div className="row detail-head">
-										<div className="col-md-10">
-											{" "}
-											Grocery
-										</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											500 INR
-										</div>
-									</div>
-									<div className="row mt-2 detail-foot ">
-										<div className="col-md-10"> Food</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											10/12/22
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<div className="card detail-card">
-								<div className="card-body">
-									<div className="row detail-head">
-										<div className="col-md-10">
-											{" "}
-											Grocery
-										</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											500 INR
-										</div>
-									</div>
-									<div className="row mt-2 detail-foot ">
-										<div className="col-md-10"> Food</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											10/12/22
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<div className="card detail-card">
-								<div className="card-body">
-									<div className="row detail-head">
-										<div className="col-md-10">
-											{" "}
-											Grocery
-										</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											500 INR
-										</div>
-									</div>
-									<div className="row mt-2 detail-foot ">
-										<div className="col-md-10"> Food</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											10/12/22
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<div className="card detail-card">
-								<div className="card-body">
-									<div className="row detail-head">
-										<div className="col-md-10">
-											{" "}
-											Grocery
-										</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											500 INR
-										</div>
-									</div>
-									<div className="row mt-2 detail-foot ">
-										<div className="col-md-10"> Food</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											10/12/22
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<div className="card detail-card">
-								<div className="card-body">
-									<div className="row detail-head">
-										<div className="col-md-10">
-											{" "}
-											Grocery
-										</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											500 INR
-										</div>
-									</div>
-									<div className="row mt-2 detail-foot ">
-										<div className="col-md-10"> Food</div>
-										<div className="col-md-2 d-flex justify-content-end">
-											10/12/22
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+						{
+						membersExpenses.map((ele) =>(
+							<Individual_card remarks={ele.remarks} type={ele.type} amount={ele.amount} date={ele.date}/>
+						))
+						}
 					</div>
 					<button>Add Expense</button>
 					<button>Log Out</button>
--- track_my_cash/server/routes/individual.js
+++ track_my_cash/server/routes/individual.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
 	getMembers,
 	showExpenses,
+	update_salary,
 } from "../controllers/Individuals.controller.js";
 
 const router = express.Router();
 
 router.get("/:id", getMembers);
 router.get("/expenses/:id", showExpenses);
+router.post("/updatesalary" , update_salary);
 
 export default router;
