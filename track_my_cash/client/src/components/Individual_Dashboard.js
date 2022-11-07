ee7f641b5adc6238a8ab3d871f9f46472d88d4eb
hussaindiwan52 <hussaindiwan52@gmail.com>
2022-11-07 15:38:02 +0530

    Revert "Revert "Temporary chart added""
    This reverts commit 5c23e820857317396828d115ccdbdf1800e3ed0d.

--- track_my_cash/client/src/components/Individual_Dashboard.js
+++ track_my_cash/client/src/components/Individual_Dashboard.js
@@ -11,6 +11,9 @@ import Cookies from "universal-cookie";
 import Update_salary from "./update_salary";
 import Add_Individual_expense from "./Add_Individual_expense";
 import Individual_card from "./Individual_card";
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const cookies = new Cookies();
 const Expenses = [];
@@ -35,7 +38,32 @@ const IndividualDashBoard = () => {
 			settypes(res.data);
 		});
 	};
-
+	const data = {
+		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		datasets: [
+		  {
+			label: '# of Votes',
+			data: [12, 19, 3, 5, 2, 3],
+			backgroundColor: [
+			  'rgba(255, 99, 132, 0.2)',
+			  'rgba(54, 162, 235, 0.2)',
+			  'rgba(255, 206, 86, 0.2)',
+			  'rgba(75, 192, 192, 0.2)',
+			  'rgba(153, 102, 255, 0.2)',
+			  'rgba(255, 159, 64, 0.2)',
+			],
+			borderColor: [
+			  'rgba(255, 99, 132, 1)',
+			  'rgba(54, 162, 235, 1)',
+			  'rgba(255, 206, 86, 1)',
+			  'rgba(75, 192, 192, 1)',
+			  'rgba(153, 102, 255, 1)',
+			  'rgba(255, 159, 64, 1)',
+			],
+			borderWidth: 1,
+		  },
+		],
+	  };
 	const fetchexpenses = async () => {
 		await axios
 			.get("http://localhost:5000/member/expenses/" + Mem_Id)
@@ -130,6 +158,7 @@ const IndividualDashBoard = () => {
 							/>
 						))}
 					</div>
+					<Doughnut data={data} />
 					<button
 						onClick={() => {
 							modalOpen
